b8c8eb4558e09b6f98b40d4e07869a47c8a23f16
Nishan46 <nipunanishan46@gmail.com>
2022-09-27 15:51:07 +0530

    today 3.51

--- Routes/index.js
+++ Routes/index.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const Router = express.Router();
 
-Router.get('/',(req,res)=>{
+
+
+Router.get('/' ,(req,res)=>{
     res.json({message:'wada wada bn ithin'})
 }),
 Router.post('/post',(req,res)=>{
-    res.json(req.body)
+    if(req.headers.host === "localhost:5000"){
+        res.json({
+            message:"You have access",
+            body:`${req.headers.accept}`
+        })
+    }
+    else{
+        res.status(700).json({message:'UnAuthorized request'}).statusMessage.replace('hellow');
+        //Hellow there
+    }
 })
 
 
--- server.js
+++ server.js
@@ -6,11 +6,15 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors());
+var Cors = {
+    origin:'http://localhost:5000'
+}
+
+app.use(cors(Cors));
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 app.use(express.text());
-app.use('/api',require('./Routes'));
+app.use('/api', require('./Routes'));
 
 app.listen(PORT, () =>console.log(`server is listening on PORT:${PORT}`));
 
