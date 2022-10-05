15d3ca74a7db6abc064a60c82cad5b4b2d4a7fa2
Nishan46 <nipunanishan46@gmail.com>
2022-10-05 11:29:52 +0530

    today 11.29 AM

--- Routes/index.js
+++ Routes/index.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const Router = express.Router();
-const axios = require('axios');
+const api = require('axios');
 const { json } = require('body-parser');
 require('dotenv').config();
 
-const api = axios.create({
-    baseURL:`${process.env.BASE_URL}`
-})
+api.defaults.baseURL = `${process.env.BASE_URL}`;
+api.defaults.timeout = 10000;
 
 
 Router.post('/member-data', async (req,res)=>{
     console.clear();
-    console.log('\n\n\n\n\n\n\n')
     const PostMember = async ()=>{
         try
         {
-            return await api.post(`/api/member-register/${process.env.API_KEY}`,req.body)
+            return await api.post(`/api/member-register/${process.env.API_KEY}`,req.body).catch((error)=>{
+                return {data:error.message , status:error.code};
+            })
         }
         catch(error)
         {
@@ -23,11 +23,14 @@ Router.post('/member-data', async (req,res)=>{
         }
     }
     const result = await PostMember();
-    const json = {
-        message : result.data,
-        code : result.status
+    if(result.data != undefined)
+    {
+        const json = {
+            message : result.data,
+            code : result.status
+        }
+        await res.json(json)
+        console.log(json)
     }
-    await res.json(json)
-    console.log(json)
 })
 module.exports = Router;
